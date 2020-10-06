68498ebe21d6729c285ae371928c4d869fbdf3c4
nurtt0949 <nurtt0949@wrdsb.ca>
2020-10-06 16:19:44 +0000

    Final balance after investment.

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Tells user their final balance after investment
+ * @author Tolga Nurtekin
  */
 public class Main {
 
@@ -9,7 +10,32 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner
+     Scanner input = new Scanner(System.in);
+    //ask user for starting balance
+    System.out.println("Please enter a starting balance");
+    //declare a variable to store strating balance
+    double balance = input.nextDouble();
+    //ask user for their intrest rate
+    System.out.println("Please enter your interest rate");
+    //declare a variable for interest rate
+    double intrest = input.nextDouble();
+     intrest = intrest/100;
+    //ask user for amount of years that they will invest
+    System.out.println("Please enter the number of years you will invest for");
+    //declare a variable for years of investment
+    double investment = input.nextDouble();
+    //create a loop 
+    for(int year = 1; year <= investment; year++){
+      //loop action:calculate the balance
+         
+         balance = balance + intrest * balance;
+      
+
+        
+    }
+     System.out.println("Your final balance would be " + balance); 
+
     
   }
 }
